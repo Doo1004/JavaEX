428ed2ef960e5d51255d75a3a1bd7363c996fa54
BIT_R34 <BIT_R34@222.106.22.83>
2020-08-04 12:11:18 +0900

    변수 연습

---
+++ JavaEx/src/com/java/basic/Ex01.java
@@ -0,0 +1,14 @@
+package com.java.basic;
+import java.util.Scanner;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		System.out.println("Input Number : ");
+		Scanner scan = new Scanner(System.in);
+		String input_a;
+		input_a = scan.nextLine();
+		
+		System.out.printf(input_a);
+	}
+}
---
+++ JavaEx/src/com/java/basic/VarEx.java
@@ -0,0 +1,18 @@
+package com.java.basic;
+
+public class VarEx {
+	public static void main(String args[]) {
+		//변수 식별자
+		//문자, 숫자, $, _의 조합
+		//다만 숫자로 시작하면 안됨
+		
+		/*
+		 * 변수명 관련
+		 * 변수명은 소문자로 작성
+		 * 단어의 조합일 경우 두 번째 단어는 대문자로 시작
+		 */
+		int myAge;		//myAge라는 이름의 int(4byte) 변수 공간을 만든다
+						//초기화
+		myAge = 24;
+	}
+}
