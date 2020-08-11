269d000668d4c2d5455f97fa4354181a7828c1ae
BIT_R34 <BIT_R34@222.106.22.83>
2020-08-11 12:49:28 +0900

    배열 연습

---
+++ JavaEx/src/com/java/basic/reftypes/WeekDay.java
@@ -0,0 +1,18 @@
+package com.java.basic.reftypes;
+
+// 열거형
+// 값의 목록의 특정 값으로 제한되어 있을 때 활용
+// java의 경우 상수, enum 많이 활용
+// enum을 활용하게 되면 
+//		1. 코드의 가독성이 높아짐
+//		2. 오류 가능성을 낮출 수 있음
+public enum WeekDay {
+	// 열거 상수 : 모두 대문자로
+	SUNDAY,
+	MONDAY,
+	TUESDAY,
+	WEDNESDAY,
+	THURSDAY,
+	FRIDAY,
+	SATURDAY,
+}
