918b7d758635b350a480f88f3da967e37e0ca5e1
BIT_R34 <BIT_R34@222.106.22.83>
2020-08-20 11:39:27 +0900

    깊은 복제, 문자열 객체, StringBuffer

---
+++ JavaEx/src/api/objectclass/v5/LangClassTest.java
@@ -0,0 +1,23 @@
+package api.objectclass.v5;
+
+
+// v5. 참조 타입 필드를 가진 클래스의 깊은 복제 테스트(v4와 코드수정 없음)
+public class LangClassTest {
+
+	public static void main(String[] args) {
+		Scoreboard s1 = new Scoreboard(new int[] {10, 20, 30 ,40});
+		System.out.println("s1 = " + s1);
+		
+		// 복제
+		Scoreboard s2 = s1.getClone();
+		System.out.println("s2 = " + s2);
+		
+		// 그렇다면 두 객체는 같은 객체인가?
+		System.out.println("s1 == s2? " + (s1 == s2));
+		
+		// 내부 참조 타입은 같은 객체인가?
+		System.out.println("s1.scores == s2.scores? " + (s1.getScores() == s2.getScores()));
+		
+		// 객체가 복제되고 내부 참조 필드도 복제되어 분리되었다(깊은 복제)
+	}
+}
