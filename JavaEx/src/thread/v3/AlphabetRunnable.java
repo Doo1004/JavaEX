2753e4c8de0ca983a623c2fbc99cdcd99fe0a21c
kwj0088 <kwj0088@hanmail.net>
2020-08-26 12:47:11 +0900

    Thread, InetAddress

---
+++ JavaEx/src/thread/v3/AlphabetRunnable.java
@@ -0,0 +1,19 @@
+package thread.v3;
+
+// Thread 생성 두 번째 방법 : Runnable 인터페이스 구현
+public class AlphabetRunnable implements Runnable {
+
+	@Override
+	public void run() {
+		// Thread 내부의 start 메서드에서 호출될 스레드의 로직
+		for (char ch = 'A'; ch <= 'Z'; ch++) {
+			System.out.println("AlphabetRunnable : " + ch);
+			// 0.3 대기
+			try {
+			Thread.sleep(300);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
